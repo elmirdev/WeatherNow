b79e1509053bbccd79a68c98ad24c0f0689e68e3
elmirdev <elmir.ismayilzada@gmail.com>
2023-06-22 16:01:17 +0400

    Remove optional

--- WeatherNow/Model/WeatherModel.swift
+++ WeatherNow/Model/WeatherModel.swift
@@ -11,7 +11,7 @@ import Foundation
 struct WeatherModel: Codable {
     let location: Location
     let current: Current
-    let forecast: ForecastModel?
+    let forecast: ForecastModel
 }
 
 // MARK: - Current
