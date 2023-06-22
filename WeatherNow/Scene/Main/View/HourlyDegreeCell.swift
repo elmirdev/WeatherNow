25dfa6a1b62855b1934870cd01224fb7b8657d1e
elmirdev <elmir.ismayilzada@gmail.com>
2023-06-22 19:40:46 +0400

    Implement hour data to HourlyDegreeCell

--- WeatherNow/Scene/Main/View/HourlyDegreeCell.swift
+++ WeatherNow/Scene/Main/View/HourlyDegreeCell.swift
@@ -9,9 +9,12 @@ import Foundation
 import SwiftUI
 
 struct HourlyDegreeCell: View {
+    let hour: Hour
+    @State private var animatableValue: CGFloat = 0
+
     var body: some View {
         VStack(spacing: 16) {
-            Text("10 AM")
+            Text(getHour(dateString: hour.time))
                 .font(.callout)
                 .foregroundColor(.gray)
             Image("sun")
@@ -23,9 +26,26 @@ struct HourlyDegreeCell: View {
                         .fill(.quaternary.opacity(0.5))
                         .frame(width:32, height: 32)
                 }
-            Text("24°")
+            TextAnimatableValue(value: animatableValue)
                 .font(.callout)
                 .fontWeight(.bold)
+        }.onAppear {
+            withAnimation(.easeInOut(duration: 2)) {
+                self.animatableValue = hour.tempC
+            }
+        }
+    }
+    
+    func getHour(dateString: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+
+        if let date = dateFormatter.date(from: dateString) {
+            let calendar = Calendar.current
+            let hour = calendar.component(.hour, from: date)
+            return "\(hour):00"
+        } else {
+            return "-"
         }
     }
 }
