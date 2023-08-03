ae5705bfe78c79c730233fabdd7ee10b97c2732f
elmirdev <elmir.ismayilzada@gmail.com>
2023-08-03 23:27:07 +0400

    Set animation to image

--- WeatherNow/Scene/Main/View/MainView.swift
+++ WeatherNow/Scene/Main/View/MainView.swift
@@ -11,6 +11,7 @@ struct MainView: View {
     
     @State var tempC: CGFloat = 0
     @ObservedObject var viewModel = MainViewModel()
+    @State var imageOffset = CGSize(width: 0, height: UIScreen.main.bounds.height)
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -25,12 +26,29 @@ struct MainView: View {
                     Image("11")
                         .resizable()
                         .scaledToFit()
-                        .frame(height: 350)
+                        .frame(width: 270, height: 270)
+                        .offset(x: imageOffset.width, y: imageOffset.height)
+                        .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                withAnimation(.easeInOut(duration: 1)) {
+                                    self.imageOffset.height = gesture.translation.height
+                                    self.imageOffset.width = gesture.translation.width
+                                }
+                            })
+                            .onEnded({ _ in
+                                withAnimation(.easeOut(duration: 1)) {
+                                    self.imageOffset = .zero
+                                }
+                            })
+                        )
+
                     TextAnimatableValue(value: tempC)
                         .foregroundColor(.white)
                         .fontWeight(.heavy)
                         .font(.system(size: 100))
-                }
+                        .offset(x: 55, y: -30)
+                }.padding()
                 StatusOfDayAndDateView(status: viewModel.weather?.current.condition.text ?? "-", date: viewModel.weather?.location.localtime ?? "-")
                 RoundedRectangleView(weather: viewModel.weather)
             }
@@ -38,6 +56,7 @@ struct MainView: View {
             viewModel.getData {
                 withAnimation(.easeInOut(duration: 2)) {
                     self.tempC = viewModel.weather?.current.tempC ?? 0
+                    imageOffset = .zero
                 }
             }
         }
