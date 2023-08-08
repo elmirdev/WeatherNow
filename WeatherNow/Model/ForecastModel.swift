70d4f97166cfd78ae80c2497f3824c1ec8221656
elmirdev <elmir.ismayilzada@gmail.com>
2023-08-08 20:45:28 +0400

    Shorten model

--- WeatherNow/Model/ForecastModel.swift
+++ WeatherNow/Model/ForecastModel.swift
@@ -82,26 +82,16 @@ struct Day: Codable {
 
 // MARK: - Hour
 struct Hour: Codable {
-    let timeEpoch: Int
     let time: String
     let tempC, tempF: CGFloat
     let isDay: Int
     let condition: Condition
     let windMph, windKph: Double
-    let windDegree: Int
-    let windDir: String
-    let pressureMB: Double
-    let pressureIn, precipMm, precipIn: Double
+    let precipMm, precipIn: Double
     let humidity, cloud: Int
-    let feelslikeC, feelslikeF, windchillC, windchillF: CGFloat
-    let heatindexC, heatindexF, dewpointC, dewpointF: CGFloat
-    let willItRain, chanceOfRain, willItSnow, chanceOfSnow: Int
-    let visKM, visMiles: Double
-    let gustMph, gustKph: Double
-    let uv: Double
+    let feelslikeC, feelslikeF: CGFloat
 
     enum CodingKeys: String, CodingKey {
-        case timeEpoch = "time_epoch"
         case time
         case tempC = "temp_c"
         case tempF = "temp_f"
@@ -109,29 +99,10 @@ struct Hour: Codable {
         case condition
         case windMph = "wind_mph"
         case windKph = "wind_kph"
-        case windDegree = "wind_degree"
-        case windDir = "wind_dir"
-        case pressureMB = "pressure_mb"
-        case pressureIn = "pressure_in"
         case precipMm = "precip_mm"
         case precipIn = "precip_in"
         case humidity, cloud
         case feelslikeC = "feelslike_c"
         case feelslikeF = "feelslike_f"
-        case windchillC = "windchill_c"
-        case windchillF = "windchill_f"
-        case heatindexC = "heatindex_c"
-        case heatindexF = "heatindex_f"
-        case dewpointC = "dewpoint_c"
-        case dewpointF = "dewpoint_f"
-        case willItRain = "will_it_rain"
-        case chanceOfRain = "chance_of_rain"
-        case willItSnow = "will_it_snow"
-        case chanceOfSnow = "chance_of_snow"
-        case visKM = "vis_km"
-        case visMiles = "vis_miles"
-        case gustMph = "gust_mph"
-        case gustKph = "gust_kph"
-        case uv
     }
 }
