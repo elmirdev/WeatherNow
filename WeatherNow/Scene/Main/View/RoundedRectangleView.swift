e434efe939f2af9d919091b499027c99de179ba2
elmirdev <elmir.ismayilzada@gmail.com>
2023-08-09 03:01:50 +0400

    Code alignment

--- WeatherNow/Scene/Main/View/RoundedRectangleView.swift
+++ WeatherNow/Scene/Main/View/RoundedRectangleView.swift
@@ -16,92 +16,92 @@ struct RoundedRectangleView: View {
     @State private var selectedDay = 0
     
     var body: some View {
-            VStack {
-                HStack {
-                    Text("Weather information")
-                        .fontWeight(.bold)
-                    Spacer()
-                }
-                .padding([.horizontal, .top])
-                HStack {
-                    VStack(alignment: .leading, spacing: 22) {
-                        if let weather {
-                            SmallIconWithTextCell(imageText: "temperature", title: "Feels Like", value: weather.current.feelslikeC, unit: "°", isExpanded: $isExpanded)
-                            SmallIconWithTextCell(imageText: "precipitation", title: "Precipitation", value: weather.current.precipIn, unit: "%", isExpanded: $isExpanded)
-                        } else {
-                            SmallIconWithTextCell(imageText: "temperature", title: "Feels Like", value: 0, unit: "°", isExpanded: $isExpanded)
-                            SmallIconWithTextCell(imageText: "precipitation", title: "Precipitation", value: 0, unit: "%", isExpanded: $isExpanded)
-                        }
-                    }
-                    Spacer()
-                    VStack(alignment: .leading, spacing: 22) {
-                        if let weather {
-                            SmallIconWithTextCell(imageText: "wind", title: "Wind Speed", value: weather.current.windKph, unit: " km/h", isExpanded: $isExpanded)
-                            SmallIconWithTextCell(imageText: "humidity", title: "Humidity", value: CGFloat(weather.current.humidity), unit: "%", isExpanded: $isExpanded)
-                        } else {
-                            SmallIconWithTextCell(imageText: "wind", title: "Wind Speed", value: 0, unit: " km/h", isExpanded: $isExpanded)
-                            SmallIconWithTextCell(imageText: "humidity", title: "Humidity", value: 0, unit: "%", isExpanded: $isExpanded)
-                        }
+        VStack {
+            HStack {
+                Text("Weather information")
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding([.horizontal, .top])
+            HStack {
+                VStack(alignment: .leading, spacing: 22) {
+                    if let weather {
+                        SmallIconWithTextCell(imageText: "temperature", title: "Feels Like", value: weather.current.feelslikeC, unit: "°", isExpanded: $isExpanded)
+                        SmallIconWithTextCell(imageText: "precipitation", title: "Precipitation", value: weather.current.precipIn, unit: "%", isExpanded: $isExpanded)
+                    } else {
+                        SmallIconWithTextCell(imageText: "temperature", title: "Feels Like", value: 0, unit: "°", isExpanded: $isExpanded)
+                        SmallIconWithTextCell(imageText: "precipitation", title: "Precipitation", value: 0, unit: "%", isExpanded: $isExpanded)
                     }
                 }
-                .padding(.horizontal, 28)
-                .padding(.vertical)
-                Divider()
-                    .padding(.horizontal)
-                
-                if let days = weather?.forecast.forecastday {
-                    HStack {
-                        ForEach(days.indices, id: \.self) { index in
-                            let day = getDayAndMonth(dateString: days[index].date).0
-                            let monthName = getDayAndMonth(dateString:days[index].date).1
-                            
-                            Text(index == 0 ? "Today" : "\(day) \(monthName)")
-                                .bold()
-                                .foregroundColor(.white)
-                                .padding(.vertical, 6)
-                                .padding(.horizontal, 8)
-                                .background {
-                                    Capsule()
-                                        .fill(colors[index])
-                                }.onTapGesture {
-                                    withAnimation(.spring()) {
-                                        for colorIndex in 0..<colors.count {
-                                            colors[colorIndex] = .gray.opacity(0.5)
-                                        }
-                                        colors[index] = .blue
-                                        selectedDay = index
+                Spacer()
+                VStack(alignment: .leading, spacing: 22) {
+                    if let weather {
+                        SmallIconWithTextCell(imageText: "wind", title: "Wind Speed", value: weather.current.windKph, unit: " km/h", isExpanded: $isExpanded)
+                        SmallIconWithTextCell(imageText: "humidity", title: "Humidity", value: CGFloat(weather.current.humidity), unit: "%", isExpanded: $isExpanded)
+                    } else {
+                        SmallIconWithTextCell(imageText: "wind", title: "Wind Speed", value: 0, unit: " km/h", isExpanded: $isExpanded)
+                        SmallIconWithTextCell(imageText: "humidity", title: "Humidity", value: 0, unit: "%", isExpanded: $isExpanded)
+                    }
+                }
+            }
+            .padding(.horizontal, 28)
+            .padding(.vertical)
+            Divider()
+                .padding(.horizontal)
+            
+            if let days = weather?.forecast.forecastday {
+                HStack {
+                    ForEach(days.indices, id: \.self) { index in
+                        let day = getDayAndMonth(dateString: days[index].date).0
+                        let monthName = getDayAndMonth(dateString:days[index].date).1
+                        
+                        Text(index == 0 ? "Today" : "\(day) \(monthName)")
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.vertical, 6)
+                            .padding(.horizontal, 8)
+                            .background {
+                                Capsule()
+                                    .fill(colors[index])
+                            }.onTapGesture {
+                                withAnimation(.spring()) {
+                                    for colorIndex in 0..<colors.count {
+                                        colors[colorIndex] = .gray.opacity(0.5)
                                     }
+                                    colors[index] = .blue
+                                    selectedDay = index
                                 }
+                            }
+                    }
+                }
+                .frame(height: isExpanded ? .none : 0)
+                .opacity(isExpanded ? 1 : 0)
+            }
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                if let hours = weather?.forecast.forecastday[selectedDay].hour {
+                    HStack(spacing: 32) {
+                        ForEach(hours.indices, id: \.self) { index in
+                            HourlyDegreeCell(hour: hours[index])
                         }
                     }
-                    .frame(height: isExpanded ? .none : 0)
-                .opacity(isExpanded ? 1 : 0)
-                }
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    if let hours = weather?.forecast.forecastday[selectedDay].hour {
-                        HStack(spacing: 32) {
-                            ForEach(hours.indices, id: \.self) { index in
-                                HourlyDegreeCell(hour: hours[index])
-                            }
-                        }
+                    .padding(.vertical)
+                    .padding(.horizontal, 28)
+                } else {
+                    HourlyDegreeCell(hour: Hour(time: "2023-08-07 00:00", tempC: 0, tempF: 0, isDay: 0, condition: Condition(text: "", icon: "day", code: 1000), windMph: 0, windKph: 0, precipMm: 0, precipIn: 0, humidity: 0, cloud: 0, feelslikeC: 0, feelslikeF: 0))
                         .padding(.vertical)
                         .padding(.horizontal, 28)
-                    } else {
-                        HourlyDegreeCell(hour: Hour(time: "2023-08-07 00:00", tempC: 0, tempF: 0, isDay: 0, condition: Condition(text: "", icon: "day", code: 1000), windMph: 0, windKph: 0, precipMm: 0, precipIn: 0, humidity: 0, cloud: 0, feelslikeC: 0, feelslikeF: 0))
-                            .padding(.vertical)
-                            .padding(.horizontal, 28)
-                    }
                 }
-            }.background {
-                RoundedRectangle(cornerRadius: 24)
-                    .fill(.white)
-            }.padding()
+            }
+        }.background {
+            RoundedRectangle(cornerRadius: 24)
+                .fill(.white)
+        }.padding()
     }
     func getDayAndMonth(dateString: String) -> (String,String) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
-
+        
         if let date = dateFormatter.date(from: dateString) {
             let calendar = Calendar.current
             
