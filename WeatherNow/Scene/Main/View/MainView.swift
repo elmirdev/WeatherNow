4148ed2308427c1c53186e21b701eff96293de5c
elmirdev <elmir.ismayilzada@gmail.com>
2023-08-10 17:35:54 +0400

    Remove min width min height

--- WeatherNow/Scene/Main/View/MainView.swift
+++ WeatherNow/Scene/Main/View/MainView.swift
@@ -41,7 +41,7 @@ struct MainView: View {
                             .resizable()
                             .scaledToFit()
                             .padding(isExpanded ? 0 : 16)
-                            .frame(minWidth: 50, maxWidth: isExpanded ? 180 : 320, minHeight: 50,maxHeight: isExpanded ? 180 : 320)
+                            .frame(maxWidth: isExpanded ? 180 : 320,maxHeight: isExpanded ? 180 : 320)
                             .offset(x: imageOffset.width, y: imageOffset.height)
                             .onTapGesture {
                                 withAnimation(.spring()) {
